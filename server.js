4282780d977ac2d4b4f193949995f23247455786
Kyle Cohen <spausek@gmail.com>
2018-02-24 11:22:38 -0800

    Added post and get to server.js to add reservations

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var path = require('path');
 var app = express();
 var PORT = 8080;
+var reservations = [];
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,11 +25,25 @@ app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname + '/reserve.html'));
 });
 
+app.get("/api/tables", function(req, res) {
+  
+
+  return reservations; 
+});
 
 
 
-app.post("/api", function(req, res) {
+app.post("/api/tables", function(req, res) {
 
+  var newReservation = req.body;
+
+  console.log(newReservation);
+
+  // We then add the json the user sent to the character array
+  reservations.push(newReservation);
+
+  // We then display the JSON to the users
+  res.json(newReservation);
 });
 
 app.listen(PORT, function() {
